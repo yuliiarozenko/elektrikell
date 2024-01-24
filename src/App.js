7df6fd2f31ca5294c9b03078c35ad8d506464927
Yuliia Rozenko <u.rozenko@gmail.com>
2024-01-24 18:34:30 +0200

    CLEAN_APP

--- src/App.js
+++ src/App.js
@@ -1,32 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-// import Test from './Test';
-import ParentComponent from './hw';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ParentComponent />
- 
-        {/* <Test firstName="Peeter" lastName="Ots" age={34}>
-          <div>Children element</div>
-        </Test> */}
-      </header>
-    </div>
+    <div></div>
   );
 }
 
-export default App;
+export default App
