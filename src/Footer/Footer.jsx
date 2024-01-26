575a50b4f829dc03f6dc40c62be040c47ff353b0
Yuliia Rozenko <u.rozenko@gmail.com>
2024-01-26 14:44:02 +0200

    change_value_butLow_butHigh

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,5 +1,14 @@
-function Footer(){
-    return (<></>);
+import React from 'react';
+import TargetHigh from './TargetHigh';
+import TargetLow from './TargetLow';
+
+function Footer({activePrice}) {
+    return (
+        <>
+            {activePrice === 'low' ? <TargetLow /> : <TargetHigh />}
+
+        </>
+    );
 }
 
 export default Footer;
