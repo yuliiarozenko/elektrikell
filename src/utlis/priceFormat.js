92950c2124cbd2a0ad82d48d161148aec470e3a0
Yuliia Rozenko <u.rozenko@gmail.com>
2024-02-07 18:23:58 +0200

    Bug fixes

--- src/utlis/priceFormat.js
+++ src/utlis/priceFormat.js
@@ -1,7 +1,7 @@
 import lodash from 'lodash';
 import { VAT, COUNTRY_CODES } from '../constant';
 
-export const mwToKw = (price) => lodash.round(parseFloat(price) / 10, 2).toFixed(2);
+export const mwToKw = (price) => +lodash.round(parseFloat(price) / 10, 2).toFixed(2);
 
 export const addTax = (amount, countryCode) => 
 lodash.round(amount * (VAT[COUNTRY_CODES[countryCode]] / 100 + 1), 2).toFixed(2);
